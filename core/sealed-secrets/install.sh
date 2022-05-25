939a7ca1b6f85d71c3678a06b5f7549c3adce6e8
Nguyen Marc <nguyen_marc@live.fr>
2022-05-25 16:37:46 +0200

    docs: styling color
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

--- core/sealed-secrets/install.sh
+++ core/sealed-secrets/install.sh
@@ -5,7 +5,7 @@ helm repo update
 
 helm upgrade --install \
   -n sealed-secrets \
-  --version 2.1.4 \
+  --version 2.1.8 \
   sealed-secrets \
   sealed-secrets/sealed-secrets \
   --create-namespace
--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -2,7 +2,7 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+const darkCodeTheme = require('prism-react-renderer/themes/palenight');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -71,7 +71,7 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: true,
+        disableSwitch: false,
         respectPrefersColorScheme: false,
       },
     }),
