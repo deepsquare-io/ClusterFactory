4b6dfd42f558ac23c55d5d30961ca63ddbb7ecb3
Darkness4 <Darkness4@users.noreply.github.com>
2022-07-31 00:10:26 +0000

    chore: update dependencies
    Signed-off-by: GitHub <noreply@github.com>

--- core.example/sealed-secrets/install.sh
+++ core.example/sealed-secrets/install.sh
@@ -5,7 +5,7 @@ helm repo update
 
 helm upgrade --install \
   -n sealed-secrets \
-  --version 2.5.0 \
+  --version 2.5.2 \
   sealed-secrets \
   sealed-secrets/sealed-secrets \
   --create-namespace
