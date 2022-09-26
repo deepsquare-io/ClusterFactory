5a8a22286cc7a3d717fe6eb7b28e28116999d6a1
Darkness4 <Darkness4@users.noreply.github.com>
2022-09-26 01:21:28 +0000

    chore: update dependencies
    Signed-off-by: GitHub <noreply@github.com>

--- core.example/argo-cd/install.sh
+++ core.example/argo-cd/install.sh
@@ -8,7 +8,7 @@ helm repo update
 helm upgrade --install \
   -n argocd \
   -f "${BASEDIR}/values.yaml" \
-  --version 5.5.4 \
+  --version 5.5.5 \
   argocd \
   argo/argo-cd \
   --create-namespace
