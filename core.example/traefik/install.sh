9425fd6915cfa07e7aa6f9cabe9fa7edcc964e94
Darkness4 <18192844+Darkness4@users.noreply.github.com>
2026-03-22 00:19:58 +0000

    chore: update dependencies
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- core.example/traefik/install.sh
+++ core.example/traefik/install.sh
@@ -8,7 +8,7 @@ helm repo update
 kubectl apply --server-side --force-conflicts -k https://github.com/traefik/traefik-helm-chart/traefik/crds/
 helm upgrade --install \
   -n traefik \
-  --version 25.0.0 \
+  --version 40.0.0-ea.1 \
   traefik \
   -f "${BASEDIR}/values.yaml" \
   traefik/traefik \
